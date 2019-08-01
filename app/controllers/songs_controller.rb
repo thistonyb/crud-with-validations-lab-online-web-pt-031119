86daea5fa2c69be07f68d33613833c0b46d13f56
Tony Baker <this.tonybaker@gmail.com>
2019-08-01 18:30:22 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -14,7 +14,8 @@ class SongsController < ApplicationController
   def create
     @song = Song.new(song_params(:title, :released,
                                  :release_year, :artist_name, :genre))
-    if !@song == nil
+    if !@song == nil 
+      @song.save
       redirect_to song_path(@song)
     else
       render :new
